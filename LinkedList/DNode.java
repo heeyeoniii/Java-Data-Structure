5d5c19f4035248566b0d4f255ef6b77935a98c76
Heeyeon Cho <90899286+heeyeoniii@users.noreply.github.com>
2021-12-17 11:55:59 +0900

    Create DNode.java

---
+++ LinkedList/DNode.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class DNode<E> {
+
+	E data;
+	DNode<E> next;
+	DNode<E> prev;
+
+	DNode(E data) {
+		this.data = data;
+		this.prev = null;
+		this.next = null;
+	}
+}
